e3ef5ee0cef6c49a9a6e21334d3453be0bf540a2
SorConf <sorcyyy@gmail.com>
2022-05-19 14:33:23 +0900

    change 変数名変更

--- mkdir.sh
+++ mkdir.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-md="${HOME}/md"
-rep="${HOME}/rep"
-sh="${HOME}/sh"
+MD="${HOME}/md"
+REP="${HOME}/rep"
+SH="${HOME}/sh"
 
-mkdir $md $rep $sh
+mkdir $MD $REP $sh
 
-mkdir $md/memo $md/blog  $md/scrap
+mkdir $MD/memo $MD/blog  $MD/scrap
 
-mkdir $rep/school $rep/indv $rep/team
+mkdir $REP/school $REP/indv $REP/team
